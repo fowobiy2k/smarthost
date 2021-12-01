0ba5640d70885723a15e8b550b9d3a948c464d6f
Oluwatosin Olufowobi <fowobiy2k@yahoo.com>
2021-12-01 16:57:02 +0100

    First commit

---
+++ SmartHost/src/test/java/com/smarthost/SmartHost/SmartHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthost.SmartHost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
